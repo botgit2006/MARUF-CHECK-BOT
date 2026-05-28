22557e9626e021801af47546b351804eeda36aae
botgit2006 <acringetiktok@gmail.com>
2026-05-29 00:24:07 +0600

    Update autoreact.js

--- Script/commands/autoreact.js
+++ Script/commands/autoreact.js
@@ -10,9 +10,9 @@ module.exports.config = {
 
 module.exports.handleEvent = async ({ api, event }) => {
  const threadData = global.data.threadData.get(event.threadID) || {};
- if (threadData["🥰"] === false) return; // Auto-react on
+ if (threadData["🥰"] === false) return; // Auto-react off
 
- const emojis = ["🥰", "😗", "🍂", "💜", "☺️", "🖤", "🤗", "😇", "🌺", "🥹", "😻", "😘", "🫣", "😽", "😺", "👀", "❤️", "🧡", "💛", "💚", "💙", "💜", "🤎", "🤍", "💫", "💦", "🫶", "🫦", "👄", "🗣️", "💏", "👨‍👩‍👦‍👦", "👨‍👨‍👦", "😵", "🥵", "🥶", "🤨", "🤐", "🫡", "🤔"];
+ const emojis = ["😱", "😗", "🍂", "💜", "☺️", "🖤", "🤗", "😇", "🌺", "🥹", "😻", "😘", "🫣", "😽", "😺", "👀", "❤️", "🧡", "💛", "💚", "💙", "💜", "🤎", "🤍", "💫", "💦", "🫶", "🫦", "👄", "🗣️", "💏", "👨‍👩‍👦‍👦", "👨‍👨‍👦", "😵", "🥵", "🥶", "🤨", "🤐", "🫡", "🤔"];
  const randomEmoji = emojis[Math.floor(Math.random() * emojis.length)];
 
  console.log(`Reacting with ${randomEmoji} to message ${event.messageID}`); // Debug log
