dcf5054fc5e3b8af279297d89f7019c3adfe99de
botgit2006 <acringetiktok@gmail.com>
2026-05-29 10:23:39 +0600

    Update Help.js

--- Script/commands/Help.js
+++ Script/commands/Help.js
@@ -144,7 +144,7 @@ ${msg}
 ┣━━━━━━━━━━━━━━━━┫
 ┃ ⚙ Prefix: ${prefix}
 ┃ 🤖 Bot Name: ${global.config.BOTNAME || "𝐒𝐡𝐚𝐡𝐚𝐝𝐚𝐭 𝐂𝐡𝐚𝐭 𝐁𝐨𝐭"}
-┃ 👑 Owner: 𝐒𝐇𝐀𝐇𝐀𝐃𝐀𝐓 𝐒𝐀𝐇𝐔
+┃ 👑 Owner: 𝐌𝐀𝐑𝐔𝐅 𝐒𝐇𝐀𝐇
 ╰━━━━━━━━━━━━━━━━╯`;
 
     downloadImages(files => {  
